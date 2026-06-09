d819a42211213a88820dcaab7baf931f20596552
Zero <huseynahmadov26@gmail.com>
2026-06-09 00:16:00 -0700

    remove SSR, switch to SPA

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -41,6 +41,14 @@ export const Route = createFileRoute("/")({
         rel: "stylesheet",
         href: "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap",
       },
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;300;400;500;600;700;800;900&display=swap",
+      },
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css2?family=Exo+2:wght@100;200;300;400;500;600;700;800;900&display=swap",
+      },
     ],
   }),
   component: Index,
